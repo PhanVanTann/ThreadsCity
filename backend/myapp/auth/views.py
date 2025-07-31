00b7ad841d2a6bfef290911da980ac84e7e62590
Phan Văn Tấn <tana2k53nvt@gmail.com>
2025-07-31 20:50:53 +0700

    login user

--- backend/myapp/auth/views.py
+++ backend/myapp/auth/views.py
@@ -3,6 +3,9 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from .services import emailService
 from utils.jwt import decode_token
+from .services import LoginService
+from utils.jwt import create_cookie
+import json
 # Create your views here.
 class EmailView(APIView):
     def get(self, request):
@@ -28,3 +31,23 @@ class EmailView(APIView):
 
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=400)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        if not email or not password:
+            return JsonResponse({"error": "Email and password are required"}, status=400)
+
+        login_service = LoginService()
+        login_response = login_service.login(email, password)
+
+        if login_response['success']:
+            access_token = login_response["access_token"]
+            refresh_token = login_response["refresh_token"]
+            response = create_cookie(access_token,refresh_token)
+            response.content = json.dumps(login_response) 
+            response['Content-Type'] = 'application/json'
+            return JsonResponse(login_response, status=200)
+        else:
+            return JsonResponse({"error": login_response['message']}, status=401)
