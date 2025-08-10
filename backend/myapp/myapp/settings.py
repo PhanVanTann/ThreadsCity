28f242169982be877a0df2e96913ac189be6272c
Phan Van Tan <tana2k53nvt@gmail.com>
2025-08-10 19:35:38 +0700

    api backend connect api ai

--- backend/myapp/myapp/settings.py
+++ backend/myapp/myapp/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'user_auth',
     'users',
     'myapp', 
+    'post',
 ]
 
 MIDDLEWARE = [
--- backend/myapp/post/views.py
+++ backend/myapp/post/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from .services import ImageCensorshipService
 
 # Create your views here.
+class ImageCensorshipView(APIView):
+    def post(self, request):
+        url = request.data.get('url')
+        postid = request.data.get('postid')
+        print("Received URL:", url, "and Post ID:", postid)
+        if not url or not postid:
+            return JsonResponse({"error": "Image URL and post ID are required"}, status=400)
+
+        image_censorship_service = ImageCensorshipService()
+
+        result = image_censorship_service.image_censorship(url, postid)
+        print("Image Censorship Result:", result)
+        if result['success']:
+            return JsonResponse(result, status=200)
+        else:
+            return JsonResponse({"error": result['error']}, status=500)
